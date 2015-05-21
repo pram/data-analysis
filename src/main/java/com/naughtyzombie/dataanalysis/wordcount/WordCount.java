322251bf6c29f04aa508fe46943a0407318afdbe
pram <pram@attale.com>
2015-05-21 11:29:52 +0100

    formatting

--- src/main/java/com/naughtyzombie/dataanalysis/wordcount/WordCount.java
+++ src/main/java/com/naughtyzombie/dataanalysis/wordcount/WordCount.java
@@ -18,14 +18,13 @@ import java.util.StringTokenizer;
 public class WordCount {
 
 
-    public static class TokenizerMapper
-            extends Mapper<Object, Text, Text, IntWritable> {
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
 
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
 
-        public void map(Object key, Text value, Context context
-        ) throws IOException, InterruptedException {
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             StringTokenizer itr = new StringTokenizer(value.toString());
             while (itr.hasMoreTokens()) {
                 word.set(itr.nextToken());
@@ -34,14 +33,11 @@ public class WordCount {
         }
     }
 
-
-    public static class IntSumReducer
-            extends Reducer<Text, IntWritable, Text, IntWritable> {
+    public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
         private IntWritable result = new IntWritable();
 
-        public void reduce(Text key, Iterable<IntWritable> values,
-                           Context context
-        ) throws IOException, InterruptedException {
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values,Context context) throws IOException, InterruptedException {
             int sum = 0;
             for (IntWritable val : values) {
                 sum += val.get();
