7a16fca72379448cf8f3e153f0ae8e5e69da0581
pram <pram@attale.com>
2015-05-22 14:15:34 +0100

    log processor

---
+++ src/main/java/com/naughtyzombie/dataanalysis/logwritable/LogProcessor.java
@@ -0,0 +1,56 @@
+package com.naughtyzombie.dataanalysis.logwritable;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+/*
+Data for this sample can be downloaded from
+ftp://ita.ee.lbl.gov/traces/NASA_access_log_Jul95.gz
+ */
+public class LogProcessor extends Configured implements Tool {
+
+    public static void main(String[] args) throws Exception {
+        int res = ToolRunner.run(new Configuration(), new LogProcessor(), args);
+        System.exit(res);
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        if (args.length < 3) {
+            System.err.println("Usage:  <input_path> <output_path> <num_reduce_tasks>");
+            System.exit(-1);
+        }
+
+		/* input parameters */
+        String inputPath = args[0];
+        String outputPath = args[1];
+        int numReduce = Integer.parseInt(args[2]);
+
+        Job job = Job.getInstance(getConf(), "log-analysis");
+
+        job.setJarByClass(LogProcessor.class);
+        job.setMapperClass(LogProcessorMap.class);
+        job.setReducerClass(LogProcessorReduce.class);
+        job.setNumReduceTasks(numReduce);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(LogWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.setInputPaths(job, new Path(inputPath));
+        FileOutputFormat.setOutputPath(job, new Path(outputPath));
+
+        int exitStatus = job.waitForCompletion(true) ? 0 : 1;
+
+        return exitStatus;
+    }
+}
