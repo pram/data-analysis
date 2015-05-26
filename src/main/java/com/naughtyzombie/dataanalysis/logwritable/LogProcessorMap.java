621683892b3c841b21a62fbc041da9f4252fffaf
pram <pram@attale.com>
2015-05-26 10:37:51 +0100

    refactoring due to changes in InputFormat

--- src/main/java/com/naughtyzombie/dataanalysis/logwritable/LogProcessorMap.java
+++ src/main/java/com/naughtyzombie/dataanalysis/logwritable/LogProcessorMap.java
@@ -1,36 +1,17 @@
 package com.naughtyzombie.dataanalysis.logwritable;
 
-import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class LogProcessorMap extends Mapper<LongWritable, Text, Text, LogWritable> {
-    LogWritable outValue = new LogWritable();
-    Text outKey = new Text();
+import java.io.IOException;
 
-    @Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String logEntryPattern = "^(\\S+) (\\S+) (\\S+) \\[([\\w:/]+\\s[+\\-]\\d{4})\\] \"(.+?)\" (\\d{3}) (\\d+)";
+public class LogProcessorMap extends Mapper<Object, LogWritable, Text, LogWritable> {
 
-        Pattern p = Pattern.compile(logEntryPattern);
-        Matcher matcher = p.matcher(value.toString());
-        if (!matcher.matches()) {
-            System.err.println("Bad Record : " + value);
-            return;
-        }
 
-        String userIP = matcher.group(1);
-        String timestamp = matcher.group(4);
-        String request = matcher.group(5);
-        int status = Integer.parseInt(matcher.group(6));
-        int bytes = Integer.parseInt(matcher.group(7));
+    public void map(Object key, LogWritable value, Context context)
+            throws IOException, InterruptedException {
 
-        outKey.set(userIP);
-        outValue.set(userIP, timestamp, request, bytes, status);
-        context.write(outKey, outValue);
+        context.write(value.getUserIP(), value);
     }
+
 }
