1e122f32ea34ca444c8c3c8649607c399762e54e
pram <pram@attale.com>
2015-09-05 17:39:06 +0100

    adding script details to insert data into hdfs from mysql

--- scripts/cloudera_tutorial/tut_ex1.sh
+++ scripts/cloudera_tutorial/tut_ex1.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+sqoop import-all-tables \
+    -m 1 \
+    --connect jdbc:mysql://quickstart:3306/retail_db \
+    --username=retail_dba \
+    --password=cloudera \
+    --compression-codec=snappy \
+    --as-avrodatafile \
+    --warehouse-dir=/user/hive/warehouse
